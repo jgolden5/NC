0d61947b084eea2d6bab5663204d01f028474d8c
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-03-21 13:21:09 -0700

    add a command to my script which shuts the fifo down rwb
    .

--- cat.sh
+++ cat.sh
@@ -1,14 +1,19 @@
 #!/usr/local/bin/bash
 
 source ~/p/bash-debugger
+fifo_name="fifo"
 
 main() {
   n=1
-  mkfifo fifo 2>/dev/null
-  while true; do
+  mkfifo $fifo_name 2>/dev/null
+  #debug
+  while [[ -p "$fifo_name" ]]; do
     echo "line #${n} = \"$(cat fifo)\""
     (( n++ ))
   done
 }
 
-main
+close_fifo() {
+  echo "closing fifo" >"$fifo_name"
+  rm "$fifo_name"
+}
