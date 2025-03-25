7fe3163b20920a545751255b9d52b7c3ac79f393
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-03-25 11:32:15 -0700

    rename fifos to request and response instead of /tmp/fifo... and remove /tmp/fifo... rwb

--- nc.sh
+++ nc.sh
@@ -3,56 +3,60 @@
 source ~/p/bash-debugger
 
 main() {
-  mkfifo /tmp/fifo 2>/dev/null #ignore "/tmp/fifo already exists" message if fifo already exists
-  exec {fd}<>/tmp/fifo_request
+  mkfifo request 2>/dev/null
+  exec {request_fd}<>request
+  mkfifo response 2>/dev/null
+  exec {response_fd}<>response
   #debug
-  nc -l 1234 <&$fd | server
+  #nc -l 1234 <&$request_fd | server >&$request_fd
+  cat <&$request_fd | server >&$response_fd
   eval "exec $fd>&-"
 }
 
 server() {
-  local response=
-  #set -x
-  while true; do
-    if [[ ! "$response" ]]; then
-      get_request || exit 1 
-      if [[ "$response" ]]; then
-        send_response >&$fd || exit 1
-        unset response
-      else
-        echo "Error: Response from request was not recognized"
-        break
-      fi
+  local \
+    line_number=1 \
+    request= \ 
+    response= \
+    response_code= \
+    response_message= \ 
+
+  set -x
+  while read line; do
+    if [[ "$line" ]]; then
+      get_request "$line" || exit 1 
+      process_request
+    else
+      send_response || exit 1
     fi
+    (( line_number++ ))
   done
-  #set +x
+  set +x
 }
 
 get_request() {
-  n=1
-  while read line; do
-    if [[ $n == 1 ]]; then
-      local method="$(echo "$line" | awk '{ print $1 }')"
-      local path="$(echo "$line" | awk '{ print $2 }')"
-      local version="$(echo "$line" | awk '{ print $3 }' | sed 's/\r//')"
-      version=${version:-"HTTP/1.0"}
-      local content_header="Content-Type: text/plain"
-      if [[ $method == "GET" ]]; then
-        if [[ $path == "/" ]]; then
-          local success_message="Netcat Succeeded"
-          response="$version 200 OK\r\n$content_header\r\n\r\n$success_message"
-        else
-          response="$version 404 Not Found\r\n$content_header\r\n\r\nPage not found"
-        fi
-      else
-        response="$version 405 Method Not Allowed\r\n$content_header\r\n\r\nMethod not allowed"
-      fi
-    elif [[ "$line" == $'\r' || ! "$line" ]]; then
-      break
+  if [[ $line_number == 1 ]]; then
+    request+="$1"
+  fi
+  echo "$line" >&2
+}
+
+process_request() {
+  local method="$(echo "$line" | awk '{ print $1 }')"
+  local path="$(echo "$line" | awk '{ print $2 }')"
+  local version="$(echo "$line" | awk '{ print $3 }' | sed 's/\r//')"
+  version=${version:-"HTTP/1.0"}
+  local content_header="Content-Type: text/plain"
+  if [[ $method == "GET" ]]; then
+    if [[ $path == "/" ]]; then
+      response_code=200
+      response_message="Netcat Succeeded"
+    else
+      response_code="404"
     fi
-    ((n++))
-    echo "$line" >&2
-  done
+  else
+    response="405 $response_message"
+  fi
 }
 
 send_response() {
