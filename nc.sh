ad4738e7c49558fe0c186b24b84e2ed89e46290a
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-03-26 09:49:00 -0700

    nc attempts to get vim_image, but only gets a tiny unrelated icon rwb

--- nc.sh
+++ nc.sh
@@ -17,6 +17,8 @@ server() {
     response_code_and_reason= \
     response_body= \
     response_count=1 \
+    should_cat_file=f \
+    cat_file=f \
 
   while read line; do
     process_request_fifo "$line"
@@ -32,9 +34,14 @@ process_request_fifo() {
     fi
     (( line_number++ ))
   else
+    set -x
     echo -e "$response"
+    if [[ "$should_cat_file" == t ]]; then
+      cat "$cat_file"
+    fi
     line_number=1
     (( response_count++ ))
+    set +x
   fi
 }
 
@@ -49,18 +56,26 @@ process_request() {
 
 generate_response() {
   if [[ $method == "GET" ]]; then
-    if [[ $path == "/" ]]; then
+    if [[ $path == "/" || $path == index.html ]]; then
       response_code_and_reason="200 OK"
-      response_body="Netcat Succeeded #${response_count}"
+      response_body=
+      content_type='image/png'
+      content_length="$(wc -c <vim_image.png | awk '{ print $1 }')"
+      should_cat_file=t
+      cat_file=vim_image.png
     else
       response_code_and_reason="404 Not Found"
       response_body="path didn't exist"
+      content_type="text/plain"
+      content_length="${#response_body}"
     fi
   else
     response_code_and_reason="405 Method Not Allowed"
     response_body="method was invalid"
+    content_type="text/plain"
+    content_length="${#response_body}"
   fi
-  response="HTTP/1.0 $response_code_and_reason\r\nContent-Type: text/plain\r\nContent-Length: ${#response_body}\r\n\r\n$response_body"
+  response="HTTP/1.0 $response_code_and_reason\r\nContent-Type: $content_type\r\nContent-Length: $content_length\r\n\r\n$response_body"
 }
 
 main
